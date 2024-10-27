4c63bb6d72e8b9dfae7bff1bdbce380a2a8e5510
def_fun <131314688+insane-infants@users.noreply.github.com>
2024-10-27 20:44:44 +0500

    minor change

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ button.addEventListener('mouseout', () => {
   button.innerHTML = 'Click Me';
 });
 
-button.addEventListener('click', () => {
-  alert('You clicked the button!');
-});
+// button.addEventListener('click', () => {
+//   alert('You clicked the button!');
+// });
 
 button.addEventListener('click', () => {
     window.location.href = 'photo.html'; // Replace 'your_target_page.html' with the actual filename
