ad3b4a8393446566e3d48ca0d9e74141af9aec3e
def_fun <131314688+insane-infants@users.noreply.github.com>
2024-11-03 10:28:42 +0500

    a 3d radar model

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ button.addEventListener('click', () => {
     window.location.href = 'photo.html'; // Replace 'your_target_page.html' with the actual filename
   });
 
+
+function displayDeviceInformation() {
+  const userAgent = navigator.userAgent;
+  const screenResolution = `${window.screen.width}x${window.screen.height}`;
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  // Display the information on the page
+  document.getElementById('userAgent').textContent = userAgent;
+  document.getElementById('screenResolution').textContent = screenResolution;
+  document.getElementById('timezone').textContent = timezone;
+}
+
+window.onload = displayDeviceInformation;
